c5154ce05634c5a7b612bfbee98e830016076f20
sara <nahlyysara@gmail.com>
2025-07-09 00:37:31 +0100

    Add SQL solutions for problems #584, #595 and #1757

---
+++ easy/1757_recyclable_and_low_fat_products.sql
@@ -0,0 +1,4 @@
+-- 1757. Recyclable and Low Fat Products
+-- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+select product_id from Products where low_fats='Y' and recyclable='Y'
---
+++ easy/584_Find_customer_referee.sql
@@ -0,0 +1,4 @@
+-- 584. Find Customer Referee
+-- Link: https://leetcode.com/problems/find-customer-referee/
+
+select name from Customer where referee_id <> 2 or referee_id is null
